03e6099f24860bdffe60a5a714a1f32ad04cb558
shaanzie <ishaanlagwankar@gmail.com>
2020-10-08 20:03:47 +0530

    Fixed latency file

---
+++ benchmarks/third_party/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/aish/leveldb/third_party/googletest/googlemock
+# Build directory: /home/aish/leveldb/benchmarks/third_party/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
